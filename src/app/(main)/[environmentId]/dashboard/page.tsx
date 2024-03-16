4eb7f139dd882c81447e61c7aac34b730cfdad58
Sourav <souravberaofficial@gmail.com>
2024-03-16 15:33:14 +0530

    middleware completed

---
+++ src/app/(main)/[environmentId]/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>DashBoard</div>;
+};
+
+export default Page;
---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+function matchPathnamePattern(pathname: string): boolean {
+  const pattern = /^\/\[([^/]+)]\/(?:dashboard|apexians|chat|projects)$/;
+  const match = pathname.match(pattern);
+
+  return match !== null && match.length > 1;
+}
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (
+    req.nextUrl.pathname.startsWith("/onboarding") ||
+    matchPathnamePattern(req.nextUrl.pathname)
+  ) {
+    if (!session) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  const emailLinkError = "Email link is invalid or has expired";
+  if (
+    req.nextUrl.searchParams.get("error_description") === emailLinkError &&
+    req.nextUrl.pathname !== "/login"
+  ) {
+    return NextResponse.redirect(
+      new URL(
+        `/login?error_description=${req.nextUrl.searchParams.get(
+          "error_description"
+        )}`,
+        req.url
+      )
+    );
+  }
+
+  if (["/login"].includes(req.nextUrl.pathname)) {
+    if (session) {
+      return NextResponse.redirect(new URL("/onboarding", req.url));
+    }
+  }
+  return res;
+}
